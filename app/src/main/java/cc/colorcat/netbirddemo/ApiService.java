d3e17876c47d0d74e1316453c9f77dc4f135831a
fireworld <xx.ch@outlook.com>
2016-12-21 23:29:47 +0800

    1. optimize

--- app/src/main/java/cc/colorcat/netbirddemo/ApiService.java
+++ app/src/main/java/cc/colorcat/netbirddemo/ApiService.java
@@ -6,6 +6,7 @@ import android.widget.ImageView;
 
 import java.util.List;
 
+import cc.colorcat.netbird.Headers;
 import cc.colorcat.netbird.NetBird;
 import cc.colorcat.netbird.Processor;
 import cc.colorcat.netbird.parser.BitmapParser;
@@ -29,63 +30,15 @@ public class ApiService {
     static {
         bird = new NetBird.Builder(baseUrl)
                 .addRequestProcessor(new Processor<Request>() {
-                    @SuppressWarnings("unchecked")
                     @NonNull
                     @Override
-                    public Request process(@NonNull Request req) {
-                        if (LogUtils.isDebug) {
-                            LogUtils.ii(TAG, "-------------------------------------- " + req.method().name() + " --------------------------------------");
-                            LogUtils.ii(TAG, "Url --> " + url(req));
-                            if (Method.POST == req.method()) {
-                                List<String> names = req.paramNames();
-                                List<String> values = req.paramValues();
-                                for (int i = 0, size = names.size(); i < size; i++) {
-                                    LogUtils.ii(TAG, "Parameter --> " + names.get(i) + " = " + values.get(i));
-                                }
-
-                                List<Request.Pack> packs = req.packs();
-                                for (int i = 0, size = packs.size(); i < size; i++) {
-                                    Request.Pack park = packs.get(i);
-                                    LogUtils.ii(TAG, "File FieldName --> " + park.name);
-                                    LogUtils.ii(TAG, "File ContentType --> " + park.contentType);
-                                    LogUtils.ii(TAG, "File Path --> " + park.file.getAbsolutePath());
-                                }
-                            }
-                            List<String> hNames = req.headerNames();
-                            List<String> hValues = req.headerValues();
-                            for (int i = 0, size = hNames.size(); i < size; i++) {
-                                LogUtils.ii(TAG, "Header --> " + hNames.get(i) + " = " + hValues.get(i));
-                            }
-                            LogUtils.ii(TAG, "----------------------------------------------------------------------------------");
-                        }
-                        return req;
-                    }
-                }).addResponseProcessor(new Processor<Response>() {
-                    @NonNull
-                    @Override
-                    public Response process(@NonNull Response response) {
-                        if (LogUtils.isDebug) {
-                            LogUtils.ii(TAG, "Response --> " + "code = " + response.code() + ", msg = " + response.msg());
-                        }
-                        return response;
+                    public Request process(@NonNull Request request) {
+                        return request.newBuilder().add("test1", "value1").add("test2", "value2").build();
                     }
                 })
+                .addRequestProcessor(new LogReqProcessor())
+                .addResponseProcessor(new LogRepProcessor())
                 .build();
-    }
-
-    private static String url(Request<?> req) {
-        String url = Utils.emptyElse(req.url(), baseUrl);
-        String path = req.path();
-        if (!Utils.isEmpty(path)) {
-            url += path;
-        }
-        if (req.method() == Method.GET) {
-            String params = req.encodedParams();
-            if (!Utils.isEmpty(params)) {
-                url = url + '?' + req.encodedParams();
-            }
-        }
-        return url;
     }
 
     public static Object call(Request<?> req) {
@@ -111,5 +64,75 @@ public class ApiService {
                     }
                 }).build();
         return call(req);
+    }
+
+    private static class LogRepProcessor implements Processor<Response> {
+        @NonNull
+        @Override
+        public Response process(@NonNull Response response) {
+            if (LogUtils.isDebug) {
+                LogUtils.ii(TAG, "-------------------------------------- response --------------------------------------");
+                LogUtils.ii(TAG, "response --> " + response.code() + "--" + response.msg());
+                Headers headers = response.headers();
+                for (int i = 0, size = headers.size(); i < size; i++) {
+                    String name = headers.name(i);
+                    String value = headers.value(i);
+                    LogUtils.ii(TAG, "response header --> " + name + " = " + value);
+                }
+                LogUtils.ii(TAG, "----------------------------------------------------------------------------------");
+            }
+            return response;
+        }
+    }
+
+    private static class LogReqProcessor implements Processor<Request> {
+        @NonNull
+        @Override
+        @SuppressWarnings("unchecked")
+        public Request process(@NonNull Request req) {
+            if (LogUtils.isDebug) {
+                Method m = req.method();
+                LogUtils.ii(TAG, "-------------------------------------- " + m.name() + " --------------------------------------");
+                String url = url(req);
+                if (m == Method.GET) {
+                    String params = req.encodedParams();
+                    if (!Utils.isEmpty(params)) {
+                        url = url + '?' + params;
+                    }
+                    LogUtils.ii(TAG, "req url --> " + url);
+                } else {
+                    LogUtils.ii(TAG, "req url --> " + url);
+                    logPairs(req.paramNames(), req.paramValues(), "parameter");
+                    logPacks(req);
+                }
+                logPairs(req.headerNames(), req.headerValues(), "header");
+                LogUtils.ii(TAG, "----------------------------------------------------------------------------------");
+            }
+            return req;
+        }
+
+        private static void logPairs(List<String> names, List<String> values, String mark) {
+            for (int i = 0, size = names.size(); i < size; i++) {
+                LogUtils.ii(TAG, "req " + mark + " -- > " + names.get(i) + " = " + values.get(i));
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        private static void logPacks(Request req) {
+            List<Request.Pack> packs = req.packs();
+            for (int i = 0, size = packs.size(); i < size; i++) {
+                Request.Pack pack = packs.get(i);
+                LogUtils.ii(TAG, "req pack --> " + pack.name + "--" + pack.contentType + "--" + pack.file.getAbsolutePath());
+            }
+        }
+
+        private static String url(Request<?> req) {
+            String url = Utils.emptyElse(req.url(), baseUrl);
+            String path = req.path();
+            if (!Utils.isEmpty(path)) {
+                url += path;
+            }
+            return url;
+        }
     }
 }
