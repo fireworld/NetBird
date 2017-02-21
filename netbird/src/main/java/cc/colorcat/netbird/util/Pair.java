4b6f82bb18c4e004683fb05df85edbbb2c577fde
fireworld <xx.ch@outlook.com>
2017-02-21 11:05:02 +0800

    1. optimize

--- netbird/src/main/java/cc/colorcat/netbird/util/Pair.java
+++ netbird/src/main/java/cc/colorcat/netbird/util/Pair.java
@@ -3,12 +3,14 @@ package cc.colorcat.netbird.util;
 import android.support.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Created by cxx on 2017/2/20.
@@ -19,7 +21,6 @@ public final class Pair {
     private boolean ignoreCase = false;
     private List<String> names;
     private List<String> values;
-    private int capacity;
 
     public Pair() {
         this(4, false);
@@ -30,7 +31,8 @@ public final class Pair {
     }
 
     public Pair(int capacity, boolean ignoreCase) {
-        this.capacity = capacity;
+        this.names = new ArrayList<>(capacity);
+        this.values = new ArrayList<>(capacity);
         this.ignoreCase = ignoreCase;
     }
 
@@ -39,7 +41,7 @@ public final class Pair {
     }
 
     public void set(String name, String value) {
-        realRemove(name);
+        realRemoveAll(name);
         realAdd(name, value);
     }
 
@@ -49,24 +51,20 @@ public final class Pair {
         }
     }
 
-    public void remove(String name) {
-        realRemove(name);
+    public void removeAll(String name) {
+        realRemoveAll(name);
     }
 
     public void clear() {
-        if (names != null) {
-            names.clear();
-            values.clear();
-        }
+        names.clear();
+        values.clear();
     }
 
     @Nullable
     public String value(String name) {
-        if (names != null) {
-            for (int i = 0, size = names.size(); i < size; i++) {
-                if (compareName(name, names.get(i))) {
-                    return values.get(i);
-                }
+        for (int i = 0, size = names.size(); i < size; i++) {
+            if (compareName(name, names.get(i))) {
+                return values.get(i);
             }
         }
         return null;
@@ -77,103 +75,69 @@ public final class Pair {
     }
 
     public List<String> values(String name) {
-        List<String> vs = null;
-        if (names != null) {
-            for (int i = 0, size = names.size(); i < size; i++) {
-                if (compareName(name, names.get(i))) {
-                    if (vs == null) vs = new ArrayList<>(2);
-                    vs.add(values.get(i));
-                }
+        List<String> result = null;
+        for (int i = 0, size = names.size(); i < size; i++) {
+            if (compareName(name, names.get(i))) {
+                if (result == null) result = new ArrayList<>(2);
+                result.add(values.get(i));
             }
         }
-        return vs != null ? Collections.unmodifiableList(vs) : Collections.<String>emptyList();
+        return result != null ? Collections.unmodifiableList(result) : Collections.<String>emptyList();
     }
 
     public List<String> names() {
-        return Utils.safeImmutableList(names);
-    }
-
-    public Set<String> nameSet() {
-        return names != null ? Collections.unmodifiableSet(new HashSet<>(names)) : Collections.<String>emptySet();
+        return Utils.immutableList(names);
     }
 
     public List<String> values() {
-        return Utils.safeImmutableList(values);
+        return Utils.immutableList(values);
+    }
+
+    public Set<String> nameSet() {
+        Set<String> result;
+        List<String> ns = new ArrayList<>(names);
+        int nullIndex = ns.indexOf(null);
+        if (nullIndex >= 0) {
+            ns.removeAll(Arrays.asList(new String[]{null}));
+        }
+        if (ignoreCase) {
+            result = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+            result.addAll(ns);
+        } else {
+            result = new TreeSet<>(ns);
+        }
+        if (nullIndex >= 0) {
+            result = new HashSet<>(result);
+            result.add(null);
+        }
+        return result;
     }
 
     public Map<String, List<String>> toMap() {
-        if (names == null) return Collections.emptyMap();
         Map<String, List<String>> result = new HashMap<>();
         for (String name : nameSet()) {
-            List<String> vs = new ArrayList<>(1);
-            for (int i = 0, size = names.size(); i < size; i++) {
-                if (Utils.equal(name, names.get(i))) {
-                    vs.add(values.get(i));
-                }
-            }
-            result.put(name, vs);
+            result.put(name, values(name));
         }
         return Collections.unmodifiableMap(result);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Pair pair = (Pair) o;
-
-        if (ignoreCase != pair.ignoreCase) return false;
-        if (names != null ? !names.equals(pair.names) : pair.names != null) return false;
-        return values != null ? values.equals(pair.values) : pair.values == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (ignoreCase ? 1 : 0);
-        result = 31 * result + (names != null ? names.hashCode() : 0);
-        result = 31 * result + (values != null ? values.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        if (names == null) {
-            return "names == null, values == null";
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0, size = names.size(); i < size; i++) {
-            sb.append(names.get(i)).append(": ").append(values.get(i)).append("\n");
-        }
-        return sb.toString();
-    }
-
     private boolean realAdd(String name, String value) {
-        if (names == null) {
-            names = new ArrayList<>(capacity);
-            values = new ArrayList<>(capacity);
-        }
         return names.add(name) && values.add(value);
     }
 
-    private void realRemove(String name) {
-        if (names != null) {
-            for (int i = names.size() - 1; i >= 0; i--) {
-                if (compareName(name, names.get(i))) {
-                    names.remove(i);
-                    values.remove(i);
-                }
+    private void realRemoveAll(String name) {
+        for (int i = names.size() - 1; i >= 0; i--) {
+            if (compareName(name, names.get(i))) {
+                names.remove(i);
+                values.remove(i);
             }
         }
     }
 
     private boolean contains(String name) {
-        if (names != null) {
-            for (int i = 0, size = names.size(); i < size; i++) {
-                if (compareName(name, names.get(i))) {
-                    return true;
-                }
+        for (int i = 0, size = names.size(); i < size; i++) {
+            if (compareName(name, names.get(i))) {
+                return true;
             }
         }
         return false;
